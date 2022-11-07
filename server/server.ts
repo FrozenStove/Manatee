32e2aceb452fe2153714cc20c99bba47da952ade
FrozenStove <Yessysutter@gmail.com>
2022-11-07 15:37:23 -0800

    Basic server setup

--- server/server.ts
+++ server/server.ts
@@ -3,5 +3,16 @@ import express from 'express'
 
 // port can be specified in the command line
 const PORT = process.env.port || 3000;
-
+const apiKey = 'bv4mnbf48v6qpate9n30';
 // fetch()
+
+const app = express();
+
+
+// app.use('api')
+
+app.use(express.static('build'))
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
