e137e196bc9103ec8f557d3a8d56d68fb929516e
FrozenStove <Yessysutter@gmail.com>
2022-11-07 17:57:04 -0800

    backend progress

--- server/controllers/finnhub.ts
+++ server/controllers/finnhub.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 const finnhub = require('finnhub');
 // import finnhub from 'finnhub'
 
@@ -10,14 +10,65 @@ const finnhubClient = new finnhub.DefaultApi()
 
 const finControllor: any = {};
 
-finControllor.getData = (req: Request, res: Response, next: NextFunction) => {
-    // Stock candles
-    finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, (error: any, data: any, response: any) => {
-        console.log(data)
-        return next()
+type SymbolResult = {
+    description: string,
+    displaySymbol: string,
+    symbol: string,
+    type: string,
+}
+
+finControllor.getData = async (req: Request, res: Response, next: NextFunction) => {
+    const symbol = req.body.symbol || 'string'
+    // console.log(symbol)
+
+    finnhubClient.quote(symbol, (error: any, data: any, response: any) => {
+        if (data.d === null){
+            next({
+                log: 'No results from entered Symbol',
+                status: 204,
+                message: { err: 'Invalid Symbol' },
+            })        
+        } else {
+            res.locals.price = data.c
+        }
     });
 
+    finnhubClient.symbolSearch(symbol, (error: any, data: any, response: any) => {
+        // the function below sorts through the incoming data and returns the object which matches the ticker symbol
+        res.locals.name = data.result.find((element: SymbolResult, index: number) => {
+            return element.symbol === symbol;
+        })
+    });
+
+    // console.log('a',a)
+
+    // return next()
 }
+
+/*
+c
+Current price
+
+d
+Change
+
+dp
+Percent change
+
+h
+High price of the day
+
+l
+Low price of the day
+
+o
+Open price of the day
+
+pc
+Previous close price
+
+
+*/
 
 
 export default finControllor
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import express from 'express'
+import express, { NextFunction, Request, Response ,Errback} from 'express'
 import finController from './controllers/finnhub';
 
 // port can be specified in the command line
@@ -8,6 +8,10 @@ const PORT = process.env.port || 3000;
 
 const app = express();
 
+// convert incoming stream data into usable objects
+app.use(express.json())
+
+
 app.use('/api/v1',
 finController.getData,
 (req, res) => {
@@ -15,6 +19,22 @@ finController.getData,
 })
 
 app.use(express.static('build'))
+
+
+// Provide local and global error handlers
+app.use((req: Request, res: Response) => res.status(404));
+
+app.use((err: Errback, req: Request, res: Response, next: NextFunction) => {
+    const defaultErr = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
